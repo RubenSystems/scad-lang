77850e7cd3e5e768e16f95099fe222f894649cd3
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2023-10-29 17:26:00 +0000

    == Parsing ==
    - Minor mods to AST for easier parsing
    - Seperated const and var declerations
    - added parsing from CST -> AST for numerics
    - added parsing from CST -> AST for constants

--- src/frontend/parser/ast_types.rs
+++ src/frontend/parser/ast_types.rs
@@ -1,6 +1,6 @@
 //===- ast_types.rs - Abstract Syntax tree -----*- rust -*-===//
 //
-// Part of the SCaD Compiler, 
+// Part of the SCaD Compiler,
 //
 //===----------------------------------------------------------------------===//
 ///
@@ -12,139 +12,169 @@
 //===----------------------------------------------------------------------===//
 
 // Definition of terminal symbols representing basic data types and identifiers
-pub struct Integer(i128); // Terminal symbol for integer values
-pub struct Float(f64); // Terminal symbol for floating-point values
+
+#[derive(Debug)]
+pub struct Integer(pub i128); // Terminal symbol for integer values
+
+#[derive(Debug)]
+pub struct Float(pub f64); // Terminal symbol for floating-point values
+
+#[derive(Debug)]
 pub struct CharArray(String); // Terminal symbol for character arrays
-pub struct Identifier(String); // Terminal symbol for identifiers
+
+#[derive(Debug)]
+pub struct Identifier(pub String); // Terminal symbol for identifiers
+
+#[derive(Debug)]
+pub struct VariableName(pub String);
+
+#[derive(Debug)]
+pub struct TypeName(pub String);
 
 // Definition of types within the language, including arrays and simple types
+#[derive(Debug)]
 pub enum Type {
     Array {
         subtype: Box<Type>, // Array type with its subtype (element type)
-        size: usize // Size of the array
+        size: usize,        // Size of the array
     },
     SimpleType {
-        identifier: String // Simple type identified by a name (identifier)
-    }
+        identifier: TypeName, // Simple type identified by a name (identifier)
+    },
 }
 
 // Definition of a block within the language, containing statements and an optional expression
+#[derive(Debug)]
 pub struct Block {
-    statements: Vec<Statement>, // List of statements within the block
-    expression: Option<Expression> // Optional expression at the end of the block
+    statements: Vec<Statement>,     // List of statements within the block
+    expression: Option<Expression>, // Optional expression at the end of the block
 }
 
 // Definition of constant declarations in the language
+#[derive(Debug)]
 pub struct ConstDecl {
-    mutable: bool, // Indicates if the constant is mutable
-    identifier: String, // Identifier for the constant
-    subtype: Type, // Type of the constant
-    expression: Expression // Expression representing the constant's value
+    pub identifier: VariableName, // Identifier for the constant
+    pub subtype: Type,            // Type of the constant
+    pub expression: Expression,   // Expression representing the constant's value
+}
+
+#[derive(Debug)]
+pub struct VariableDecl {
+    identifier: VariableName, // Identifier for the constant
+    subtype: Type,            // Type of the constant
+    expression: Expression,   // Expression representing the constant's value
 }
 
 // Definition of a conditional block in the language
+#[derive(Debug)]
 pub struct ConditionalBlock {
     condition: BooleanExpression, // Condition for the block (boolean expression)
-    block: Block // Block of statements to be executed if the condition is met
+    block: Block,                 // Block of statements to be executed if the condition is met
 }
 
 // Enumeration of numeric operations supported in the language
-enum NumericOp {
-    Add, // Addition operation
+#[derive(Debug)]
+pub enum NumericOp {
+    Add,      // Addition operation
     Subtract, // Subtraction operation
     Multiply, // Multiplication operation
-    Divide // Division operation
+    Divide,   // Division operation
 }
 
 // Enumeration of different representations for numeric values
-pub enum NumericRepr {
-    Int(Integer), // Representation of an integer value
-    Float(Float), // Representation of a floating-point value
-    NumericExpression(NumericExpression), // Representation of a complex numeric expression
-    Identifier(Identifier) // Representation of an identifier referring to a numeric value
-}
+// #[derive(Debug)]
+// pub enum NumericRepr {
+//     Int(Integer),                         // Representation of an integer value
+//     Float(Float),                         // Representation of a floating-point value
+//     NumericExpression(NumericExpression), // Representation of a complex numeric expression
+//     Identifier(Identifier), // Representation of an identifier referring to a numeric value
+// }
 
-// Definition of a single atomic numeric expression
-pub struct NumericAtom {
-    unary_minus: bool, // Indicates if the numeric value is negated
-    numeric_expression: NumericRepr // The numeric representation
-}
-
-// Definition of a numeric expression consisting of an atomic value and a series of right-hand side operands
+#[derive(Debug)]
 pub struct NumericExpression {
-    atom: Box<NumericAtom>, // Atomic numeric value
-    rhss: Vec<NumericAtom> // List of right-hand side operands
+    pub lhs: Box<Expression>,
+    pub op: NumericOp,
+    pub rhs: Box<Expression>,
 }
 
 // Enumeration of logical 'AND' and 'OR' operators
+#[derive(Debug)]
 pub enum BooleanJoins {
     And, // Logical 'AND' operator
-    Or // Logical 'OR' operator
+    Or,  // Logical 'OR' operator
 }
 
 // Enumeration of binary boolean operations for comparisons
+#[derive(Debug)]
 pub enum BinaryBooleanOp {
-    LessThanEqual, // Less than or equal comparison
+    LessThanEqual,     // Less than or equal comparison
     GreatherThanEqual, // Greater than or equal comparison
-    GreaterThan, // Greater than comparison
-    LessThan, // Less than comparison
-    Equality // Equality comparison
+    GreaterThan,       // Greater than comparison
+    LessThan,          // Less than comparison
+    Equality,          // Equality comparison
 }
 
 // Enumeration of different comparators in binary boolean expressions
+#[derive(Debug)]
 pub enum BinaryBooleanComparitor {
-    Int(Integer), // Comparator representing an integer value
-    Float(Float), // Comparator representing a floating-point value
-    Identifier(Identifier) // Comparator representing an identifier referring to a boolean value
+    Int(Integer),           // Comparator representing an integer value
+    Float(Float),           // Comparator representing a floating-point value
+    Identifier(Identifier), // Comparator representing an identifier referring to a boolean value
 }
 
 // Enumeration of representations for boolean values and expressions
+#[derive(Debug)]
 pub enum BooleanRepr {
-    Int(Integer), // Representation of an integer value as a boolean
-    Float(Float), // Representation of a float value as a boolean
-    True, // Boolean 'true' value
-    False, // Boolean 'false' value
+    Int(Integer),           // Representation of an integer value as a boolean
+    Float(Float),           // Representation of a float value as a boolean
+    True,                   // Boolean 'true' value
+    False,                  // Boolean 'false' value
     Identifier(Identifier), // Representation of an identifier referring to a boolean value
     BinaryBooleanExpression {
         lhs: BinaryBooleanComparitor, // Left-hand side comparator in the binary boolean expression
-        op: BinaryBooleanOp, // Operator for the comparison
-        rhs: BinaryBooleanComparitor // Right-hand side comparator in the binary boolean expression
-    }
+        op: BinaryBooleanOp,          // Operator for the comparison
+        rhs: BinaryBooleanComparitor, // Right-hand side comparator in the binary boolean expression
+    },
 }
 
 // Definition of a single atomic boolean expression
+#[derive(Debug)]
 pub struct BooleanAtom {
-    not: bool, // Indicates if the boolean value is negated
-    repr: BooleanRepr // The boolean representation
+    not: bool,         // Indicates if the boolean value is negated
+    repr: BooleanRepr, // The boolean representation
 }
 
 // Definition of a boolean expression consisting of an atomic value and a series of right-hand side atoms
+#[derive(Debug)]
 pub struct BooleanExpression {
-    repr: BooleanAtom, // Atomic boolean value
-    rhss: Vec<(BooleanJoins, BooleanAtom)> // List of right-hand side atoms combined with boolean joins
+    repr: BooleanAtom,                      // Atomic boolean value
+    rhss: Vec<(BooleanJoins, BooleanAtom)>, // List of right-hand side atoms combined with boolean joins
 }
 
 // Enumeration of major building blocks of the language expressions
+#[derive(Debug)]
 pub enum Expression {
     BooleanExpression(BooleanExpression), // Boolean expressions
     NumericExpression(NumericExpression), // Numeric expressions
-    Float(Float), // Floating-point values
-    Integer(Integer), // Integer values
-    CharArray(CharArray), // Character arrays
-    Identifier(Identifier) // Identifiers
+    Float(Float),                         // Floating-point values
+    Integer(Integer),                     // Integer values
+    CharArray(CharArray),                 // Character arrays
+    Identifier(Identifier),               // Identifiers
 }
 
 // Enumeration of different types of statements in the language
+#[derive(Debug)]
 pub enum Statement {
-    ConstDecl(ConstDecl), // Constant declaration statement
+    ConstDecl(ConstDecl),       // Constant declaration statement
+    VariableDecl(VariableDecl), // Constant declaration statement
     IfControlFlow {
-        if_block: ConditionalBlock, // If condition block
+        if_block: ConditionalBlock,      // If condition block
         else_ifs: Vec<ConditionalBlock>, // List of else-if condition blocks
-        else_block: Option<Block> // Optional else block if no conditions are met
+        else_block: Option<Block>,       // Optional else block if no conditions are met
     },
     Loop {
         condition: Option<BooleanExpression>, // Optional condition for loop execution
-        block: Block // Block of statements within the loop
+        block: Block,                         // Block of statements within the loop
     },
-    Expression(Expression) // Expression statement
+    Expression(Expression), // Expression statement
 }
--- src/frontend/parser/scad_parser.rs
+++ src/frontend/parser/scad_parser.rs
@@ -1,5 +1,13 @@
-use pest::{pratt_parser::{PrattParser, Assoc, Op}, iterators::Pairs};
+use pest::{
+    iterators::Pairs,
+    pratt_parser::{Assoc, Op, PrattParser},
+};
 use pest_derive::Parser;
+
+use super::ast_types::{
+    ConstDecl, Expression, Float, Identifier, Integer, NumericExpression, NumericOp, Statement,
+    Type, TypeName, VariableName,
+};
 
 // use super::ast_types::{Numeric, Expression};
 
@@ -7,91 +15,171 @@ use pest_derive::Parser;
 #[grammar = "frontend/parser/scad.pest"]
 pub struct SCADParser;
 
-
 pub struct ParserToAST {
-	parser: PrattParser<Rule>
+    parser: PrattParser<Rule>,
 }
 
 impl ParserToAST {
-	pub fn new() -> Self {
-		Self {
-			parser: PrattParser::new()
-				.op(Op::infix(Rule::add, Assoc::Left) | Op::infix(Rule::subtract, Assoc::Left))
-				.op(Op::infix(Rule::multiply, Assoc::Left) | Op::infix(Rule::divide, Assoc::Left))
-				.op(Op::prefix(Rule::unary_minus))
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            parser: PrattParser::new()
+                .op(Op::infix(Rule::add, Assoc::Left) | Op::infix(Rule::subtract, Assoc::Left))
+                .op(Op::infix(Rule::multiply, Assoc::Left) | Op::infix(Rule::divide, Assoc::Left))
+                .op(Op::prefix(Rule::unary_minus)),
+        }
+    }
 }
-/* 
+
 impl ParserToAST {
-	pub fn parse(&self, rules: Pairs<Rule>) { 
+    fn parse_type(&self, tpe: pest::iterators::Pair<'_, Rule>) -> Type {
+        match tpe.as_rule() {
+            Rule::r#type => self.parse_type(tpe),
+            Rule::simple_type => Type::SimpleType {
+                identifier: TypeName(tpe.as_str().into()),
+            },
+            Rule::array_type => {
+                println!("GOT A TYPE {tpe:?}");
+                let mut p = tpe.into_inner();
+                let typename = self.parse_type(p.next().unwrap().into());
+                let size = p.next().unwrap().as_str().parse::<usize>().unwrap();
+                Type::Array {
+                    subtype: Box::new(typename),
+                    size: size,
+                }
+            }
+            _ => unreachable!("PANICCCCCCCC invalid type"),
+        }
+    }
 
-		self.parser
-        .map_primary(|primary| match primary.as_rule() {
-            Rule::integer => Numeric::Integer(primary.as_str().parse::<i128>().unwrap()),
-            Rule::float => Numeric::Float(primary.as_str().parse::<f64>().unwrap()),
-            Rule::char_array => Expression::CharArray(primary.as_str()),
-            Rule::identifier => todo!(),
-            Rule::boolean_t => todo!(),
-            Rule::r#type => todo!(),
-            Rule::array_type => todo!(),
-            Rule::simple_type => todo!(),
-            Rule::mutable_modifier => todo!(),
-            Rule::const_decl => todo!(),
-            Rule::block => todo!(),
-            Rule::if_control_flow => todo!(),
-            Rule::if_block => todo!(),
-            Rule::else_if_block => todo!(),
-            Rule::else_block => todo!(),
-            Rule::r#loop => todo!(),
-            Rule::numeric_op => todo!(),
-            Rule::add => todo!(),
-            Rule::subtract => todo!(),
-            Rule::multiply => todo!(),
-            Rule::divide => todo!(),
-            Rule::unary_minus => todo!(),
-            Rule::numeric => todo!(),
-            Rule::numeric_atom => todo!(),
-            Rule::numeric_expression => todo!(),
-            Rule::unary_boolean_op => todo!(),
-            Rule::not => todo!(),
-            Rule::boolean_joins => todo!(),
-            Rule::and => todo!(),
-            Rule::or => todo!(),
-            Rule::binary_boolean_op => todo!(),
-            Rule::greater_than => todo!(),
-            Rule::less_than => todo!(),
-            Rule::equality => todo!(),
-            Rule::less_than_equal => todo!(),
-            Rule::greather_than_equal => todo!(),
-            Rule::binary_boolean_comparitors => todo!(),
-            Rule::binary_boolean_expression => todo!(),
-            Rule::boolean => todo!(),
-            Rule::boolean_atom => todo!(),
-            Rule::boolean_expression => todo!(),
-            Rule::expression => todo!(),
-            Rule::statement => todo!(),
-            Rule::statements => todo!(),
-            Rule::program => todo!(),
-            // Rule::int  => primary.as_str().parse().unwrap(),
-            // Rule::expr => parse_expr(primary.into_inner(), pratt), // from "(" ~ expr ~ ")"
-            _          => unreachable!(),
-        })
-        .map_prefix(|op, rhs| match op.as_rule() {
-            Rule::unary_minus  => -rhs,
-            _          => unreachable!(),
-        })
-        .map_infix(|lhs, op, rhs| match op.as_rule() {
-            Rule::add  => lhs + rhs,
-            Rule::sub  => lhs - rhs,
-            Rule::mul  => lhs * rhs,
-            Rule::div  => lhs / rhs,
-            Rule::pow  => (1..rhs+1).map(|_| lhs).product(),
-            _          => unreachable!(),
-        })
-        .parse(pairs);
+    pub fn parse(&self, rules: Pairs<Rule>) -> Statement {
+        self.parser
+            .map_primary(
+                |primary: pest::iterators::Pair<'_, Rule>| match primary.as_rule() {
+                    Rule::integer => Statement::Expression(Expression::Integer(Integer(
+                        primary.as_str().parse::<i128>().unwrap(),
+                    ))),
+                    Rule::float => Statement::Expression(Expression::Float(Float(
+                        primary.as_str().parse::<f64>().unwrap(),
+                    ))),
+                    // Rule::char_array => todo!(),
+                    // Rule::identifier => todo!(),
+                    // Rule::boolean_t => todo!(),
 
-	}
+                    // Rule::mutable_modifier => todo!(),
+                    Rule::const_decl => {
+                        let mut p = primary.into_inner();
+                        let identifier = VariableName(p.next().unwrap().as_str().into());
+                        let stype_unparsed = p.next().unwrap();
+                        let parsed_stype = self.parse_type(stype_unparsed);
+
+                        let Statement::Expression(expression) =
+                            self.parse(p.next().unwrap().into_inner())
+                        else {
+                            unreachable!("NOT AN EXPRESSION!");
+                        };
+
+                        Statement::ConstDecl(ConstDecl {
+                            identifier: identifier,
+                            subtype: parsed_stype,
+                            expression: expression,
+                        })
+                    }
+                    // Rule::block => todo!(),
+                    // Rule::if_control_flow => todo!(),
+                    // Rule::if_block => todo!(),
+                    // Rule::else_if_block => todo!(),
+                    // Rule::else_block => todo!(),
+                    // Rule::r#loop => todo!(),
+                    // Rule::numeric_op => todo!(),
+                    // Rule::add => todo!(),
+                    // Rule::subtract => todo!(),
+                    // Rule::multiply => todo!(),
+                    // Rule::divide => todo!(),
+                    // Rule::unary_minus => todo!(),
+                    // Rule::numeric => todo!(),
+                    Rule::numeric_atom => self.parse(primary.into_inner()),
+                    Rule::numeric_expression => self.parse(primary.into_inner()),
+                    // Rule::unary_boolean_op => todo!(),
+                    // Rule::not => todo!(),
+                    // Rule::boolean_joins => todo!(),
+                    // Rule::and => todo!(),
+                    // Rule::or => todo!(),
+                    // Rule::binary_boolean_op => todo!(),
+                    // Rule::greater_than => todo!(),
+                    // Rule::less_than => todo!(),
+                    // Rule::equality => todo!(),
+                    // Rule::less_than_equal => todo!(),
+                    // Rule::greather_than_equal => todo!(),
+                    // Rule::binary_boolean_comparitors => todo!(),
+                    // Rule::binary_boolean_expression => todo!(),
+                    // Rule::boolean => todo!(),
+                    // Rule::boolean_atom => todo!(),
+                    // Rule::boolean_expression => todo!(),
+                    Rule::expression => self.parse(primary.into_inner()),
+                    Rule::statement => self.parse(primary.into_inner()),
+                    Rule::statements => self.parse(primary.into_inner()),
+                    // Rule::program => todo!(),
+                    rule => {
+                        eprintln!("{}", primary);
+                        unreachable!("Expr::parse expected atom, found {:?}", rule);
+                    }
+                },
+            )
+            // .map_prefix(|op, rhs| match op.as_rule() {
+            //     Rule::unary_minus  => -rhs,
+            //     _          => unreachable!(),
+            // })
+            .map_infix(|lhs, op, rhs| match op.as_rule() {
+                Rule::add => {
+                    let (Statement::Expression(lhs), Statement::Expression(rhs)) = (lhs, rhs)
+                    else {
+                        unreachable!();
+                    };
+
+                    Statement::Expression(Expression::NumericExpression(NumericExpression {
+                        lhs: Box::new(lhs),
+                        op: NumericOp::Add,
+                        rhs: Box::new(rhs),
+                    }))
+                }
+                Rule::subtract => {
+                    let (Statement::Expression(lhs), Statement::Expression(rhs)) = (lhs, rhs)
+                    else {
+                        unreachable!();
+                    };
+
+                    Statement::Expression(Expression::NumericExpression(NumericExpression {
+                        lhs: Box::new(lhs),
+                        op: NumericOp::Subtract,
+                        rhs: Box::new(rhs),
+                    }))
+                }
+                Rule::multiply => {
+                    let (Statement::Expression(lhs), Statement::Expression(rhs)) = (lhs, rhs)
+                    else {
+                        unreachable!();
+                    };
+
+                    Statement::Expression(Expression::NumericExpression(NumericExpression {
+                        lhs: Box::new(lhs),
+                        op: NumericOp::Subtract,
+                        rhs: Box::new(rhs),
+                    }))
+                }
+                Rule::divide => {
+                    let (Statement::Expression(lhs), Statement::Expression(rhs)) = (lhs, rhs)
+                    else {
+                        unreachable!();
+                    };
+
+                    Statement::Expression(Expression::NumericExpression(NumericExpression {
+                        lhs: Box::new(lhs),
+                        op: NumericOp::Divide,
+                        rhs: Box::new(rhs),
+                    }))
+                }
+                rule => unreachable!("Can't parse infix operator {:?}", rule),
+            })
+            .parse(rules)
+    }
 }
-
-*/
