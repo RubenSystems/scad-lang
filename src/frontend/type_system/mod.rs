ce9b28c2f013fdb88ef45ab0bf211b974a92193a
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2024-01-09 20:24:07 +0000

    Basic polymorphic functions

--- src/frontend/type_system/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod context;
-pub mod substitution;
-pub mod tir_ast_expressions;
-pub mod tir_types;
-pub mod traits;
-pub mod type_engine;
-pub mod mir_to_tir;
--- test_programs/basic/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int num = 2 + 4; 
-	printf("%i\n", num);
-	return 0;
-}
